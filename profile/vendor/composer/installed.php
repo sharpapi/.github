023e1147db3f366e52a810fddc5d28cf2a44feb6
makowskid <makowskid@users.noreply.github.com>
2025-07-27 01:44:06 +0000

    Updated latest blog posts

--- profile/vendor/composer/installed.php
+++ profile/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'sharpapi/sharpapi',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '5a33ef21781aa659db59abbb3360f4333d3bc262',
+        'reference' => '4138a640a66f80877e8c717a03f618ff299d057a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'sharpapi/sharpapi' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '5a33ef21781aa659db59abbb3360f4333d3bc262',
+            'reference' => '4138a640a66f80877e8c717a03f618ff299d057a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
